5e9e7f86d30fe7087e403a045c9b445ebf0d6f7e
Ubah Abdi <Ubahabdi183@gmail.com>
2022-09-08 11:42:33 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the input function
+*
+* description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* return: 0
+*/
+int main(void)
+{
+        printf("with proper grammar, but is a piece of art,\n");
+        return (0);
+}
