84dfe70e808e8331972211500b0f7dd530d6fd1a
Ubah Abdi <Ubahabdi183@gmail.com>
2022-09-08 14:20:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,10 @@
 *
 * description: using the main function
 * this program prints "Programming is like building a multilingual puzzle"
-* return: 0
+* return:0
 */
 int main(void)
-{
-
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+{ 
+puts'("\"Programming is like building a multilingual puzzle");
+return(0);
 }
