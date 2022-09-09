2311fa4004d815d6917df813ae8c69403b97ea94
Ubah Abdi <Ubahabdi183@gmail.com>
2022-09-09 04:56:33 -0700

    positive_or_negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main () {
+int n;
+printf("enter a number\n");
+scanf:("%d;n\n");
+      if(n==0)
+	printf("0");
+      if(n>0)
+	printf("postive number");
+      else
+        printf("negative number");
+return 0;      
+}
