5176db58865be82df8e6089bbf5c44889158fbdf
Ubah Abdi <Ubahabdi183@gmail.com>
2022-09-10 10:52:48 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - print the string in the input function
- *
- * description: using the main function                                                                                                  
- * this program prints "Programming is like building a multilingual puzzle
- * return: 0 
+ * description: using the main function
+ * Return: 0
 */
 int main(void)
 {
@@ -14,9 +12,9 @@ int i;
 long li;
 long long lli;
 float f;
-printf( size of char:%ld bytes(s)\n" (sizeof(c));
-printf( size of char:%ld bytes(s)\n" (sizeof(i));	
-printf( size of char:%ld bytes(s)\n" (sizeof(li);	
-printf( size of char:%ld bytes(s)\n" (sizeof(lli));
-return: 0
+printf("size of char : % ld bytes(s)\n", (sizeof(c)));
+printf("size of int : % ld bytes(s)\n", (sizeof(i)));
+printf("size of long : % ld bytes(s)\n", (sizeof(li)));
+printf("size of long long : % ld bytes(s)\n", (sizeof(lli)));	
+return (0);
 }
