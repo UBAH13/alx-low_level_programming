046ee88c79ec52f727658110d2df9dd3fc6a32f5
Ubah Abdi <Ubahabdi183@gmail.com>
2022-09-10 07:40:59 -0700

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
 /**
 * main - print the string in the input function
 *
-* description: using the main function
+* description:using the main function
 * this program prints "Programming is like building a multilingual puzzle
-* return: 0
+* return: 0;
 */
 int main(void)
 {
