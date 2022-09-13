ee27f4c2d7b481679868a84a2a05cd982cf5204a
Ubah Abdi <Ubahabdi183@gmail.com>
2022-09-13 04:48:17 -0700

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "putchar";
+
+	while (*sh)
+	{
+		putchar(*sh);
+		sh++;
+	}
+	putchar('\n');
+	return (0);
+}
