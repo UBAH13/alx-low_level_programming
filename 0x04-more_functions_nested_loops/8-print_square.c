5436d507bed31cd994af03f17c99c92ef1a49af4
Ubah Abdi <Ubahabdi183@gmail.com>
2022-09-15 10:08:13 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include <stdio>
+#include "main.h"
+/**
+ * print_square - print a square using the character
+ *
+ * @size: is the size of the square
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+int length = size;
+int width = size;
+if (size <= 0)
+_putchar('\n');
+else
+{
+while (length > 0)
+{
+width = size;
+while (width > 0)
+{
+_putchar('#');
+width--;
+}
+length--;
+_putchar('\n');
+}
+}
+}
