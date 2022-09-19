27ed8508e17efad9a5eb55c8c1c8aea67fc4e0e1
Ubah Abdi <Ubahabdi183@gmail.com>
2022-09-19 09:15:05 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,16 @@
  */
 void rev_string(char *s)
 {
-int i, c, k;
-char *a, aux;
-a = s;
-while (s[c] != '\0')
+char rev = s[0];
+int fcounter = 0;
+int i;
+while (s[fcounter] != '\0')
+fcounter++;
+for (i = 0; i < fcounter; i++)
 {
-c++;
-}
-for (k = 1; k < c; k++)
-{
-a++;
-}
-for (i = 0; i < (c / 2); i++)
-{
-aux = s[i];
-s[i] = *a;
-*a = aux;
-a--;
+fcounter--;
+rev = s[i];
+s[i] = s[fcounter];
+s[fcounter] = rev;
 }
 }
