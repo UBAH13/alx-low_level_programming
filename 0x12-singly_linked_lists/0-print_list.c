9c8f8b196aa8e4b4542258a2ce28896a40f852e8
Ubah Abdi <Ubahabdi183@gmail.com>
2022-10-20 13:18:30 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,28 @@
+/**
+ *  File: 0-print_list.c
+ *  Auth: ubah
+ */
+
 #include <stdio.h>
 #include "lists.h"
+
 /**
  *  print_listint - Prints all the elements of a listint_t list.
  *  @h: A pointer to the head of the list_t list.
  *
  *  Return: The number of nodes in the list_t list.
  */
-size_t print_listint(const listint_t *h)
+size_t print_list(const list_t *h)
 {
-size_t cont = 0;
+size_t nodes = 0;
 while (h)
 {
-if (h->str)
-printf("[%u] %s\n", h->len, h->str);
-else
+if (h->str == NULL)
 printf("[0] (nil)\n");
-cont++;
+else
+printf("[%d] %s\n", h->len, h->str);
+nodes++;
 h = h->next;
 }
-return (cont);
+return (nodes);
 }
